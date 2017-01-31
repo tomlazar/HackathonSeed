2df5dfb56f2478eec1fcc4704cdf240d2ebc8c51
Duckworth, Kyle <kyle.duckworth@ge.com>
2017-01-31 11:07:54 -0600

    Update sample.js

--- Java Server/src/main/webapp/sample.js
+++ Java Server/src/main/webapp/sample.js
@@ -2,35 +2,46 @@
 // Define a new module for our app
 var app = angular.module('predixModule', []);
 
-// The main controller for the sonar issue tracker
+// The main controller for the Predix Hackathon
 app.controller('mainController', function($scope, $http) {
-	// Add the function generateIssueList to the scope
-	// This will be called on page load, and the apply filters button
+	// Add the function getDataFromPredix to the scope
+	// This will be called on page load
     $scope.getDataFromPredix = getDataFromPredix;
-
+    
+    /*
+     * Get data from Predix - send the http request that will be mapped in our controller
+     */
     function getDataFromPredix(){
-    	$http.get('/getAuthToken')
+    	// Get the UAA authentication token
+    	$http.get('/springmvc-helloworld/getAuthToken')
     	
-    	// If the request was successful, parse the response
-    	.success(function(authToken) {			
+    	// If the request was successful
+    	.success(function(authToken) {	
+    		// Log auth token in console
     		console.log(authToken);
-    		$http.post('/getData/')
+    		// Get data from predix using the token
+    		$http.post('/springmvc-helloworld/getData/')
         	
-        	// If the request was successful, parse the response
+        	// If the request was successful
         	.success(function(data) {			
+        		// Log the data in console
         		console.log(data);
         	})
+        	// Error from getData request
         	.error(function(data) {
+        		// Log the error in console
         		console.log("Could not get the Predix data due to " + data);
         	}); 
     	})
+    	// Error from getAuthToken request
     	.error(function(data) {
+    		// Log error in console
     		console.log("Could not get the Auth Token due to " + data);
     	}); 
     	
     	
     }
-
+    // Call method on page load
   	getDataFromPredix(); 
  
        
